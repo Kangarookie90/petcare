ee1e576db6a5eb33e6b649bf7a09033f3ea58e4d
Sinestro90 <simone.calliari@gmail.com>
2026-04-20 22:53:28 +0200

    Aggiunto Vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { inizializzaSync } from './syncService.js'
+import { inject } from '@vercel/analytics'
+
+// Avvia analytics
+inject()
 
 // Avvia sync all'avvio dell'app
 inizializzaSync();
